ac4901bdbf5f9174ff8dc6e771b5184674cb71fa
cristianrojasalexan <cristianrojas.ca@gmail.com>
2020-12-09 00:16:04 -0300

    :sparkles: new methods

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,16 @@ class Route < ApplicationRecord
   belongs_to :driver, optional: true
 
   enum load_types: { "General": 0, "Refrigerada": 1 }
+
+  def self.unassigned_routes
+    Route.where(driver_id: nil, vehicle: nil)
+  end
+
+  def self.drivers_ids
+    Route.all.pluck(:driver_id)
+  end
+
+  def self.drivers_with_route
+    Route.where.not(driver_id: nil).pluck(:driver_id)
+  end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,9 @@ class Vehicle < ApplicationRecord
   has_many :routes
 
   enum load_types: { "General": 0, "Refrigerada": 1 }
+
+  def self.unassigned_vehicles
+    assigned_vehicles = Route.where.not(vehicle_id: nil).pluck(:vehicle_id)
+    Vehicle.where.not(id: assigned_vehicles)
+  end
 end
